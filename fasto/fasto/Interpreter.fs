3fd9fe856729bd3d158f74994f0a8384456ed80f
Helga Rykov Ibsen <92053112+helgarykov@users.noreply.github.com>
2023-05-17 11:52:40 +0200

    interpreter done

--- fasto/fasto/Interpreter.fs
+++ fasto/fasto/Interpreter.fs
@@ -144,18 +144,60 @@ let rec evalExp (e : UntypedExp, vtab : VarTable, ftab : FunTable) : Value =
         e.g., `And (e1, e2, pos)` should not evaluate `e2` if `e1` already
               evaluates to false.
   *)
-  | Times(_, _, _) ->
-        failwith "Unimplemented interpretation of multiplication"
-  | Divide(_, _, _) ->
-        failwith "Unimplemented interpretation of division"
-  | And (_, _, _) ->
-        failwith "Unimplemented interpretation of &&"
-  | Or (_, _, _) ->
-        failwith "Unimplemented interpretation of ||"
-  | Not(_, _) ->
-        failwith "Unimplemented interpretation of not"
-  | Negate(_, _) ->
-        failwith "Unimplemented interpretation of negate"
+  | Times(e1, e2, pos) ->
+       let res1 = evalExp(e1, vtab, ftab)
+       let res2 = evalExp(e2, vtab, ftab)
+       match (res1, res2) with
+         | (IntVal n1, IntVal n2) -> IntVal (n1 * n2)
+         | (IntVal _, _) -> reportWrongType "right operand of *" Int res2 (expPos e2)
+         | (_, _) -> reportWrongType "left operand of *" Int res1 (expPos e1)
+
+  | Divide(e1, e2, pos) ->
+      let res1 = evalExp(e1, vtab, ftab)
+      let res2 = evalExp(e2, vtab, ftab)
+      match (res1, res2) with
+        | (IntVal n1, IntVal n2) -> 
+            if n2 = 0 then 
+                  failwith "Error: Division by zero" 
+            else 
+                  IntVal (n1 / n2)
+        | (IntVal _, _) -> reportWrongType "right operand of /" Int res2 (expPos e2)
+        | (_, _) -> reportWrongType "left operand of /" Int res1 (expPos e1)
+
+  | And (e1, e2, pos) ->
+      let res1 = evalExp(e1, vtab, ftab)
+      match res1 with
+        | BoolVal false -> BoolVal false              // If e1 is false, return false without evaluating e2
+        | BoolVal true ->                             // If e1 is true, then evaluate e2
+            let res2 = evalExp(e2, vtab, ftab)
+            match res2 with
+              | BoolVal b -> BoolVal b                  // Return the result of e2
+              | _ -> reportWrongType "right operand of &&" Bool res2 (expPos e2)
+        | _ -> reportWrongType "left operand of &&" Bool res1 (expPos e1)
+
+  | Or (e1, e2, position) ->
+      let res1 = evalExp(e1, vtab, ftab)
+      match res1 with
+        | BoolVal true -> BoolVal true                  // If e1 is true, return true without evaluating e2
+        | BoolVal false ->                              // If e1 is false, then evaluate e2
+            let res2 = evalExp(e2, vtab, ftab)
+            match res2 with 
+              | BoolVal b -> BoolVal b
+              | _ -> reportWrongType "right operand of &&" Bool res2 (expPos e2)
+        | _ -> reportWrongType "left operand of &&" Bool res1 (expPos e1)
+
+  | Not (e, pos) ->
+    let res = evalExp(e, vtab, ftab)
+    match res with
+        | BoolVal b -> BoolVal (not b)                                 // If e1 is a boolean, flip its value
+        | _ -> reportWrongType "operand of not" Bool res (expPos e)  // If e1 is not a boolean, report an error
+
+  | Negate(e, pos) ->
+    let res = evalExp(e, vtab, ftab)
+    match res with
+    | IntVal n -> IntVal (-n)
+    | _ -> reportWrongType "operand of negate" Int res (expPos e)
+
   | Equal(e1, e2, pos) ->
         let r1 = evalExp(e1, vtab, ftab)
         let r2 = evalExp(e2, vtab, ftab)
